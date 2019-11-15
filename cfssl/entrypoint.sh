040b696f46f5db663ae9b6bd0a3645fe294c186e
Frank Binder <frank.binder@serialize.de>
2019-11-16 00:26:59 +0100

    [MOD] parameter handling

--- cfssl/entrypoint.sh
+++ cfssl/entrypoint.sh
@@ -2,77 +2,72 @@
 
 set -e
 
-args=()
+PARAMS=()
 
-if [ -z "${CFSSL_DB_PASSWORD}" ]; then
+function handle_db_config
+{
+    if [ -z "${CFSSL_DB_PASSWORD}" ]; then
 
-    db_pw="${CFSSL_DB_PASSWORD}"
-    db_host="${CFSSL_DB_HOSTNAME:-db}"
-    db_port="${CFSSL_DB_PORT:-5432}"
-    db_user="${CFSSL_DB_HOSTNAME:-postgres}"
-    db_name="${CFSSL_DB_NAME:-postgres}"
+        db_pw="${CFSSL_DB_PASSWORD}"
+        db_host="${CFSSL_DB_HOSTNAME:-db}"
+        db_port="${CFSSL_DB_PORT:-5432}"
+        db_user="${CFSSL_DB_HOSTNAME:-postgres}"
+        db_name="${CFSSL_DB_NAME:-postgres}"
 
-    db_uri="postgres://${db_user}:${db_pw}@${db_host}:${db_port}/${db_name}?sslmode=disable"
+        db_uri="postgres://${db_user}:${db_pw}@${db_host}:${db_port}/${db_name}?sslmode=disable"
 
-    {
-        echo '{'
-        echo '  "driver": "postgres",'
-        echo '  "data_source": "$db_uri"'
-        echo '}'
-    }  > /etc/cfssl/db-config.json
+        {
+            echo '{'
+            echo '  "driver": "postgres",'
+            echo '  "data_source": "$db_uri"'
+            echo '}'
+        }  > /etc/cfssl/db-config.json
 
-fi
+    fi
+}
+
+
+function check_value_param
+{
+    if [[ ! -z "${2}" ]]; then
+        PARAMS+=("-${1}=${2}")
+    fi
+}
+
+
+function check_file_param
+{
+    if [[ ! -z "${2}" || -f "${2}" ]]; then
+        PARAMS+=("-${1}=${2}")
+    fi
+}
+
 
 
 address="${CFSSL_ADDRESS:-0.0.0.0}"
 port="${CFSSL_PORT:-8888}"
+
+check_value_param "address" "${CFSSL_ADDRESS}"
+check_value_param "port" "${CFSSL_PORT}"
+
+
 ca_cert="${CFSSL_CA_CERT:-/etc/cfssl/ca.pem}"
 ca_key="${CFSSL_CA_KEY:-/etc/cfssl/ca-key.pem}"
-config="${CFSSL_CONFIG:-/etc/cfssl/config.json}"
-db_config="${CFSSL_DB_CONFIG:-/etc/cfssl/db-config.json}"
 ca_bundle="${CFSSL_CA_BUNDLE:-/etc/cfssl/ca-bundle.crt}"
 int_bundle="${CFSSL_INT_BUNDLE:-/etc/cfssl/int-bundle.crt}"
 
-if [ "$2" == "serve" ]; then
+config="${CFSSL_CONFIG:-/etc/cfssl/config.json}"
+db_config="${CFSSL_DB_CONFIG:-/etc/cfssl/db-config.json}"
 
-    if [[ $@ != *'-address='* ]] && [[ $@ != *'-address '* ]] && [[ -f "${address}" ]]; then
-        args+=("-address=${address}")
-    fi
+check_file_param "ca" "${CFSSL_CA_CERT}"
+check_file_param "ca-key" "${CFSSL_CA_KEY}"
+check_file_param "ca-bundle" "${CFSSL_CA_BUNDLE}"
+check_file_param "int-bundle" "${CFSSL_INT_BUNDLE}"
 
-    if [[ $@ != *'-port='* ]] && [[ $@ != *'-port '* ]] && [[ -f "${port}" ]]; then
-        args+=("-port=${port}")
-    fi
-fi
+check_file_param "config" "${CFSSL_CONFIG}"
+check_file_param "db-config" "${CFSSL_DB_CONFIG}"
 
-if [ "$1" == "cfssl" ]; then
 
-    if [[ $@ != *'-ca='* ]] && [[ $@ != *'-ca '* ]] && [[ -f "${ca_cert}" ]]; then
-        args+=("-ca=${ca_cert}")
-    fi
 
-    if [[ $@ != *'-ca-key='* ]] && [[ $@ != *'-ca-key '* ]] && [[ -f "${ca_key}" ]]; then
-        args+=("-ca-key=${ca_key}")
-    fi
-
-    if [[ $@ != *'-config='* ]] && [[ $@ != *'-config '* ]] && [[ -f "${config}" ]]; then
-        args+=("-config=${config}")
-    fi
-
-    if [[ $@ != *'-db-config='* ]] && [[ $@ != *'-db-config '* ]] && [[ -f "${db_config}" ]]; then
-        args+=("-db-config=${db_config}")
-    fi
-
-    if [[ $@ != *'-ca-bundle='* ]] && [[ $@ != *'-ca-bundle '* ]] && [[ -f "${ca_bundle}" ]]; then
-        args+=("-ca-bundle=${ca_bundle}")
-    fi
-
-    if [[ $@ != *'-int-bundle='* ]] && [[ $@ != *'-int-bundle '* ]] && [[ -f "${int_bundle}" ]]; then
-        args+=("-inr-bundle=${int_bundle}")
-    fi
-
-fi
-
-echo "${args[@]}"
-
-exec "$@" "${args[@]}"
+exec "$@" "${PARAMS[@]}"
 
