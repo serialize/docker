cf7d99f6fca2dc67a58ab865a995685c089aafee
Frank Binder <frank.binder@serialize.de>
2019-11-29 22:35:14 +0100

    [MOD] lemur static hostname fix

--- lemur/entrypoint.sh
+++ lemur/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+LEMUR_API_HOSTNAME="${LEMUR_API_HOSTNAME:-pki_api}"
+
+sed -i "s/localhost/${LEMUR_API_HOSTNAME}/g" /opt/lemur/gulp/server.js && \
+    
+
 LEMUR_PASSWORD="${LEMUR_PASSWORD:-lemur}"
 
 lemur init -p "$LEMUR_PASSWORD"
