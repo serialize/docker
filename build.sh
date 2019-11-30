11f297bf8411e43ae7191853f0ac8e9cbaf1f813
Frank Binder <frank.binder@serialize.de>
2019-11-30 06:09:58 +0100

    [MOD] build script can handle tags

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-docker build --rm -t serialize/$1 $1/
-docker push serialize/$1
+NAME="serialize/${1}"
+FOLDER="${1}/"
+
+if [ $# -eq 2 ]; then
+    NAME="serialize/${1}:${2}"
+    FOLDER="${1}/${2}/"
+fi
+
+echo "----------------------------------"
+echo "Building ${NAME}..."
+echo "----------------------------------"
+docker build --rm -t "${NAME}" "${FOLDER}"
+echo ""
+echo "----------------------------------"
+echo "Pushing ${NAME}..."
+echo "----------------------------------"
+docker push "${NAME}"
